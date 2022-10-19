ac7dd0f06aa6982f785829637394fa18cba1e21e
Matheus Araújo <souzamatheus8174@gmail.com>
2022-10-19 15:03:59 -0300

    chore: update prettier config at .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	plugins: [require('prettier-plugin-tailwindcss')],
+
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'ignore',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+  embeddedLanguageFormatting: 'off',
+};

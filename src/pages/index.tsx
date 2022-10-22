c627a4d81ff3c5a48795f93a82264f3427eda86a
Matheus Araújo <souzamatheus8174@gmail.com>
2022-10-21 22:30:53 -0300

    refactor: refactor home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => (
-  <h1 className="bg-green-400 dark:bg-slate-800">Hello world</h1>
+  <div className="flex h-screen w-screen flex-col items-center justify-center gap-10 bg-black text-white">
+    <h1 className="text-3xl font-bold text-white">NEXT BOILERPLATE</h1>
+
+    <div className="flex flex-col items-center justify-center gap-2">
+      <h2 className="text-2xl text-white">
+        Typescript, Tailwind and Storybook
+      </h2>
+
+      <p>
+        if you like it, dont forget to{' '}
+        <a
+          className="text-blue-500"
+          href="https://github.com/Matheus8174/boilerplate-sb-next-tailwind-typescript"
+        >
+          give an star
+        </a>
+      </p>
+    </div>
+  </div>
 );
 
 export default Home;

ae267831fecfc9d2b0ad3cecfa65fe726447a686
Matheus Araújo <souzamatheus8174@gmail.com>
2022-11-23 13:32:51 -0300

    chore: consfirure dark light mode at storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import ToggleThemeButton from '../components/ToggleThemeButton';
 
 import theme from '../styles/theme';
 
@@ -15,11 +17,34 @@ export const parameters = {
   },
 };
 
-export const decorators = [
-  (Story) => (
+export const globalTypes = {
+  direction: {
+    name: 'Direction',
+    description: 'Direction for layout',
+    defaultValue: 'LTR',
+    toolbar: {
+      icon: 'globe',
+      items: ['LTR', 'RTL'],
+    },
+  },
+};
+
+const withChakra = (StoryFn, context) => {
+  const { direction } = context.globals;
+  const dir = direction.toLowerCase();
+
+  React.useEffect(() => {
+    document.documentElement.dir = dir;
+  }, [dir]);
+
+  return (
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <Story />
+      <div dir={dir} id="story-wrapper" style={{ minHeight: '100vh' }}>
+        <ToggleThemeButton />
+        <StoryFn />
+      </div>
     </ChakraProvider>
-  ),
-];
+  );
+};
+
+export const decorators = [withChakra];

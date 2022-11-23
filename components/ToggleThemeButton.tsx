4232f33405a717b2d523b593737877909167ec0c
Matheus Araújo <souzamatheus8174@gmail.com>
2022-11-23 13:33:25 -0300

    feat: create toggle theme button

---
+++ components/ToggleThemeButton.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import React from 'react';
+
+import { useColorMode, Box } from '@chakra-ui/react';
+
+import toggleLightDark from '@public/lottie/toggle-light-dark.json';
+import useLottieAnimation from 'hooks/useLottieAnimation';
+
+const ToggleThemeButton: React.FC = () => {
+  const { ref, lottie } = useLottieAnimation({
+    animationData: toggleLightDark,
+    initialSegment: [0, 35],
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+  });
+
+  const { toggleColorMode } = useColorMode();
+
+  const toggleIconTheme = React.useRef(false);
+
+  function handleClick() {
+    toggleColorMode();
+
+    lottie?.setDirection(toggleIconTheme.current ? -1 : 1);
+
+    lottie?.setSpeed(1.8);
+    lottie?.play();
+
+    toggleIconTheme.current = !toggleIconTheme.current;
+  }
+
+  return (
+    <Box width="20" cursor="pointer" ref={ref} onClick={() => handleClick()} />
+  );
+};
+
+export default ToggleThemeButton;

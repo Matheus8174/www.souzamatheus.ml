080c555a0531bd4d7ceaa88df658234e7d55ea56
Matheus Araújo <souzamatheus8174@gmail.com>
2022-11-23 23:15:52 -0300

    feat: add prismic Provider and Preview

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -10,41 +10,50 @@ import {
   theme as chakraTheme,
 } from '@chakra-ui/react';
 
-import Header from 'src/components/Header';
-import Footer from 'src/components/Footer';
+import Link from 'next/link';
+import { PrismicProvider } from '@prismicio/react';
+import { PrismicPreview } from '@prismicio/next';
 
-import theme from 'src/styles/theme';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+
+import theme from '@styles/theme';
+import { repositoryName } from '../lib/prismicio';
 
 function Providers({ children }: PropsWithChildren) {
   const { space } = chakraTheme;
 
   return (
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <Grid
-        h="100vh"
-        gridTemplateRows={`${space['20']} 1fr ${space['20']}`}
-        gridTemplateColumns="1fr 1fr 1fr"
-        templateAreas={`
+    <PrismicProvider internalLinkComponent={Link}>
+      <PrismicPreview repositoryName={repositoryName}>
+        <ChakraProvider theme={theme}>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Grid
+            h="100vh"
+            gridTemplateRows={`${space['20']} 1fr ${space['20']}`}
+            gridTemplateColumns="1fr 1fr 1fr"
+            templateAreas={`
           "header header header"
           "content content content"
           "footer footer footer"
         `}
-        rowGap="6"
-      >
-        <GridItem area="header" as="header">
-          <Header />
-        </GridItem>
-        <GridItem area="content" as="main">
-          <Stack align="center" justify="center" h="full" w="100%">
-            {children}
-          </Stack>
-        </GridItem>
-        <GridItem area="footer" as="footer">
-          <Footer />
-        </GridItem>
-      </Grid>
-    </ChakraProvider>
+            rowGap="6"
+          >
+            <GridItem area="header" as="header">
+              <Header />
+            </GridItem>
+            <GridItem area="content" as="main">
+              <Stack align="center" justify="center" h="full" w="100%">
+                {children}
+              </Stack>
+            </GridItem>
+            <GridItem area="footer" as="footer">
+              <Footer />
+            </GridItem>
+          </Grid>
+        </ChakraProvider>
+      </PrismicPreview>
+    </PrismicProvider>
   );
 }
 

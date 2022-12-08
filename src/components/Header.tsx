3896ab4922f0357af0343cbcb443993e7ef91149
Matheus Araújo <souzamatheus8174@gmail.com>
2022-12-08 09:39:13 -0300

    refactor: move Header to pages folder

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,4 @@
-'use client';
-
 import React from 'react';
-import Link from 'next/link';
 
 import {
   useColorMode,
@@ -16,15 +13,15 @@ import {
   Box,
   Flex,
   Text,
-  useBreakpoint,
+  HStack,
 } from '@chakra-ui/react';
 import { FiHome, FiPhoneCall, FiBook } from 'react-icons/fi';
 
+import { PrismicLink } from '@prismicio/react';
 import MenuAnimation from '../animations/MenuAnimation';
-// import ToggleThemeButton from './ToggleThemeButton';
+import ToggleThemeButton from './ToggleThemeButton';
 
 const Header: React.FC = () => {
-  const currentViewSize = useBreakpoint();
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const [hoverAnimation, setHoverAnimation] = React.useState(false);
 
@@ -35,23 +32,18 @@ const Header: React.FC = () => {
     { id: 3, item: 'Contato', icon: FiPhoneCall, to: '/contact' },
   ];
 
-  const isMobile: { [x: string]: boolean } = {
-    sm: true,
-    md: true,
-    base: true,
-  };
-
   const isDark = colorMode === 'dark';
 
   return (
     <Flex
+      w="full"
       h="full"
       align="center"
       justify="center"
       color={isDark ? 'white' : 'gray.900'}
     >
       <Stack direction="row" w="90%" justify="space-between" align="center">
-        <Link href="/">
+        <PrismicLink href="/">
           <Stack direction="row" cursor="pointer">
             <Stack
               direction="row"
@@ -67,94 +59,110 @@ const Header: React.FC = () => {
               </Text>
             </Stack>
           </Stack>
-        </Link>
+        </PrismicLink>
 
-        {isMobile[currentViewSize] ? (
-          <Flex align="center" justify="center">
-            <Box
-              rounded="full"
-              zIndex="-1"
-              w="40px"
-              h="40px"
-              bgColor={isDark ? 'gray.700' : ''}
-              pos="absolute"
-              display={hoverAnimation ? 'block' : 'none'}
-            />
-            <Menu
-              isOpen={isMenuOpen}
-              onClose={() => setIsMenuOpen(false)}
-              onOpen={() => setIsMenuOpen(true)}
+        <Flex
+          align="center"
+          justify="center"
+          display={{
+            base: 'flex',
+            md: 'none',
+          }}
+        >
+          <Box
+            rounded="full"
+            zIndex="-1"
+            w="40px"
+            h="40px"
+            bgColor={isDark ? 'gray.700' : ''}
+            pos="absolute"
+            display={hoverAnimation ? 'block' : 'none'}
+          />
+          <Menu
+            isOpen={isMenuOpen}
+            onClose={() => setIsMenuOpen(false)}
+            onOpen={() => setIsMenuOpen(true)}
+          >
+            <MenuButton
+              aria-label="Options"
+              onMouseEnter={() => setHoverAnimation(true)}
+              onMouseLeave={() => setHoverAnimation(false)}
             >
-              <MenuButton
-                aria-label="Options"
-                onMouseEnter={() => setHoverAnimation(true)}
-                onMouseLeave={() => setHoverAnimation(false)}
-              >
-                <Stack align="center" justify="center">
-                  <Stack w="60px" h="60px" align="center">
-                    <MenuAnimation isMenuOpen={isMenuOpen} />
-                  </Stack>
+              <Stack align="center" justify="center">
+                <Stack w="60px" h="60px" align="center">
+                  <MenuAnimation isMenuOpen={isMenuOpen} />
                 </Stack>
-              </MenuButton>
-              <MenuList
-                bg={isDark ? 'gray.800' : 'white'}
-                border="none"
-                boxShadow={isDark ? 'white 1px 2px 12px' : 'gray 1px 2px 12px'}
-              >
-                {menuItems.map(({ item, icon: Icon, to, id }) => (
-                  <Link href={to} key={id}>
-                    <MenuItem
-                      fontWeight="bold"
-                      icon={<Icon fontSize="15" />}
-                      bg={isDark ? 'gray.800' : 'white'}
-                      _hover={{
-                        bgColor: isDark ? 'gray.900' : 'gray.50',
-                      }}
-                    >
-                      {item}
-                    </MenuItem>
-                  </Link>
-                ))}
-              </MenuList>
-            </Menu>
-          </Flex>
-        ) : (
-          <List as="nav">
-            <Stack
-              spacing="4"
-              direction="row"
-              as="ul"
-              align="center"
-              justify="center"
+              </Stack>
+            </MenuButton>
+            <MenuList
+              bg={isDark ? 'gray.800' : 'white'}
+              border="none"
+              boxShadow={isDark ? 'white 1px 2px 12px' : 'gray 1px 2px 12px'}
             >
-              {menuItems.map(({ item, icon, to, id }) => (
-                <Link href={to} key={id}>
-                  <ListItem
-                    fontSize="lg"
+              {menuItems.map(({ item, icon: Icon, to, id }) => (
+                <PrismicLink href={to} key={id}>
+                  <MenuItem
                     fontWeight="bold"
-                    display="flex"
-                    alignItems="center"
-                    justifyItems="center"
-                    transition="background-color .2s"
-                    px="4"
-                    py="1"
-                    rounded="base"
-                    _hover={{ bgColor: isDark ? 'gray.700' : 'gray.50' }}
+                    icon={<Icon fontSize="15" />}
+                    bg={isDark ? 'gray.800' : 'white'}
+                    _hover={{
+                      bgColor: isDark ? 'gray.900' : 'gray.50',
+                    }}
                   >
-                    <ListIcon
-                      as={icon}
-                      color={isDark ? 'pink.500' : 'pink.600'}
-                    />
-                    <Text>{item.toUpperCase()}</Text>
-                  </ListItem>
-                </Link>
+                    {item}
+                  </MenuItem>
+                </PrismicLink>
               ))}
-            </Stack>
-          </List>
-        )}
-      </Stack>
+              <HStack align="center" pl="2" pt="1">
+                <ToggleThemeButton />
+              </HStack>
+            </MenuList>
+          </Menu>
+        </Flex>
 
-      {/* <ToggleThemeButton /> */}
+        <List
+          as="nav"
+          display={{
+            base: 'none',
+            md: 'block',
+          }}
+        >
+          <Stack
+            as="ul"
+            direction="row"
+            spacing="4"
+            justify="center"
+            align="center"
+          >
+            <Box display={{ base: 'none', lg: 'block' }}>
+              <ToggleThemeButton />
+            </Box>
+
+            {menuItems.map(({ item, icon, to, id }) => (
+              <PrismicLink href={to} key={id}>
+                <ListItem
+                  fontSize="lg"
+                  fontWeight="bold"
+                  display="flex"
+                  alignItems="center"
+                  justifyItems="center"
+                  transition="background-color .2s"
+                  px="4"
+                  py="1"
+                  rounded="base"
+                  _hover={{ bgColor: isDark ? 'gray.700' : 'gray.50' }}
+                >
+                  <ListIcon
+                    as={icon}
+                    color={isDark ? 'pink.500' : 'pink.600'}
+                  />
+                  <Text>{item.toUpperCase()}</Text>
+                </ListItem>
+              </PrismicLink>
+            ))}
+          </Stack>
+        </List>
+      </Stack>
     </Flex>
   );
 };

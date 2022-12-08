4746c94c6dd419a147389ab365c787007445a574
Matheus Araújo <souzamatheus8174@gmail.com>
2022-12-07 23:47:50 -0300

    chore: create preismic files

---
+++ .slicemachine/assets/slices/PostParagrah/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/PostParagrah';
+
+export default {
+  title: 'slices/PostParagrah'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading2","text":"Community","spans":[]}],"description":[{"type":"paragraph","text":"Consectetur excepteur sunt officia et cupidatat consequat nulla tempor eu. Occaecat quis dolore Lorem consectetur pariatur officia ipsum amet.","spans":[]}]},"slice_type":"post_paragrah","id":"_Default"}} />
+_Default.storyName = ''
